172c9736bad4af520dc033fbbfd28e0c7baeeab1
andkoehlert <Andkoehlert@outlook.com>
2025-03-07 19:58:50 +0100

    correct link for cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 export function startServer() {
  
   app.use(cors({
-    origin: 'https://mongo-api-2025.onrender.com',
+    origin: 'https://mevn2025.onrender.com',
     credentials: true,
     methods: 'GET, PUT, POST, DELETE',
     allowedHeaders: ['auth-token', 'Origin', 'X-Requested-Width', 'Content-Type', 'Accepts'],
